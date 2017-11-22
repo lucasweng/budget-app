61742bc21c716d6ae0f8f08e330534adef9ff19d
Lucas Weng <30640930+lucasweng@users.noreply.github.com>
2017-11-22 13:09:26 +0800

    Tested Higher Order Component feature

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/playground/hoc.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
